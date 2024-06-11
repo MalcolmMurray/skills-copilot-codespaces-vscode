bf3b9266a268a9d12e2eeda5f8dc88a3ce4bdef4
Dr Malcolm Murray <malcolm.murray@gmail.com>
2024-06-11 17:01:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var skills = {
+        frontend: ['HTML', 'CSS', 'JavaScript', 'React', 'Angular'],
+        backend: ['Node', 'Express', 'PHP', 'Python'],
+        database: ['MongoDB', 'MySQL', 'PostgreSQL'],
+        devops: ['Docker', 'Kubernetes', 'Jenkins', 'CircleCI'],
+        cloud: ['AWS', 'Google Cloud', 'Azure'],
+        tools: ['Git', 'Jira', 'Confluence', 'Slack'],
+    };
+    return skills;
+}
